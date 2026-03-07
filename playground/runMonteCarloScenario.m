883809854c748e0d6704c03dfa7dfe2b4b92a92b
Alex Cooke <30250508@blackpool.ac.uk>
2026-03-07 17:12:26 +0000

    Added implementation for MVC-1 (including diagrams, scenario configuration, figures, etc.)
    Started on MVC-2

--- playground/runMonteCarloScenario.m
+++ playground/runMonteCarloScenario.m
@@ -1,7 +1,7 @@
 %% runBehaviouralScenario.m
 clear; clc;
 
-scenarioId = "MCV-1";   % choose: MCV-1, MCV-2, MCV-3, (MCV-4?)
+scenarioId = "MCV-2";   % choose: MCV-1, MCV-2, MCV-3, (MCV-4?)
 
 %% Simulation parameters
 N = 1000;       % Number of trials
@@ -9,6 +9,16 @@ Fs = 10;
 dt = 1/Fs;
 Tsim = 20;
 t = 0:dt:Tsim;
+
+% Number of runs where warn/fault occurs during at least one timestep
+warnOccured = zeros(N, 1); 
+faultOccured = zeros(N, 1);
+
+warnEvents = 0;
+faultEvents = 0;
+
+warnPresent = zeros(1, length(t)); 
+warnTimesteps = zeros(N, length(t)); 
 
 %% Instantiate sensors
 for run = 1:N
@@ -22,24 +32,190 @@ for run = 1:N
     oilSensor   = OilPressureSensorSITL("Fs", Fs);
     
     %% Default: no faults
-    altSensor.Injector   = DeterministicInjector();
-    vsSensor.Injector    = DeterministicInjector();
-    asSensor.Injector    = DeterministicInjector();
-    pitchSensor.Injector = DeterministicInjector();
-    rollSensor.Injector  = DeterministicInjector();
-    tempSensor.Injector  = DeterministicInjector();
-    oilSensor.Injector   = DeterministicInjector();
-    
-    %% Apply scenario-specific scripted fault
     switch scenarioId
         case "MCV-1"
-            % Do some stuff
+            altSensor.Injector   = SimpleInjector();
+            vsSensor.Injector    = SimpleInjector();
+            asSensor.Injector    = SimpleInjector();
+            pitchSensor.Injector = SimpleInjector();
+            rollSensor.Injector  = SimpleInjector();
+            tempSensor.Injector  = SimpleInjector();
+            oilSensor.Injector   = SimpleInjector();
 
         case "MCV-2"
-            % Do some stuff
+            altSensor.Injector   = DeterministicInjector();
+            vsSensor.Injector    = DeterministicInjector();
+            asSensor.Injector    = DeterministicInjector();
+            pitchSensor.Injector = DeterministicInjector();
+            rollSensor.Injector  = DeterministicInjector();
+            tempSensor.Injector  = DeterministicInjector();
+            oilSensor.Injector   = DeterministicInjector();
 
         case "MCV-3"
             % Do some stuff
+    end
+
+    %% Model for noise
+    NoiseMu = 0; 
+    NoiseSigma = NaN; % empty for now - decided by scenario 
+    
+    %% Apply scenario-specific noise
+    switch scenarioId
+        case "MCV-1"
+            NoiseSigma = 0.3; 
+            altSensor.Injector = SimpleInjector(...
+                "NoiseSigma", 0.3,...
+                "FaultRatePerSecond", 0.0...
+                ); 
+
+            vsSensor.Injector = SimpleInjector(...
+                "NoiseSigma", 0.1,...
+                "FaultRatePerSecond", 0.0...
+                ); 
+
+            asSensor.Injector = SimpleInjector(...
+                "NoiseSigma", 0.1,...
+                "FaultRatePerSecond", 0.0...
+                ); 
+
+            pitchSensor.Injector = SimpleInjector(...
+                "NoiseSigma", 0.3,...
+                "FaultRatePerSecond", 0.0...
+                ); 
+
+            rollSensor.Injector = SimpleInjector(...
+                "NoiseSigma", 0.3,...
+                "FaultRatePerSecond", 0.0...
+                ); 
+
+            tempSensor.Injector = SimpleInjector(...
+                "NoiseSigma", 0.5,...
+                "FaultRatePerSecond", 0.0...
+                ); 
+
+            oilSensor.Injector = SimpleInjector(...
+                "NoiseSigma", 0.2,...
+                "FaultRatePerSecond", 0.0...
+                ); 
+
+        case "MCV-2"
+            failureClasses = ["BIAS", "DRIFT", "STUCK", "DROPOUT"];
+            idx = randi(length(failureClasses));
+            FaultType = failureClasses(idx);
+            
+            commonArgs = { ...
+                "NoiseSigma", 0.3, ...
+                "Enabled", true, ...
+                "FaultType", FaultType, ...
+                "FaultStartTime", 5.0, ...
+                "FaultDuration", 0.8 ...
+            };
+            
+            specificArgs = {};
+            
+            switch FaultType
+                case "BIAS"
+                    specificArgs = { ...
+                        "BiasMagnitude", 1 + 4*rand() ...
+                    };
+            
+                case "DRIFT"
+                    specificArgs = { ...
+                        "DriftRateMagnitude", 0.5 + 2*rand() ...
+                    };
+            
+                case "SPIKE"
+                    specificArgs = { ...
+                        "SpikeMagnitude", 5 + 15*rand() ...
+                    };
+            
+                case "DROPOUT"
+                    specificArgs = { ...
+                        "DropoutAsNaN", true ...
+                    };
+            
+                case "STUCK"
+                    specificArgs = {};
+            end
+
+            altSensor.Injector = DeterministicInjector(commonArgs{:}, specificArgs{:});
+            vsSensor.Injector = DeterministicInjector(commonArgs{:}, specificArgs{:});
+            asSensor.Injector = DeterministicInjector(commonArgs{:}, specificArgs{:}); 
+            pitchSensor.Injector = DeterministicInjector(commonArgs{:}, specificArgs{:});
+            rollSensor.Injector = DeterministicInjector(commonArgs{:}, specificArgs{:});
+            tempSensor.Injector = DeterministicInjector(commonArgs{:}, specificArgs{:});
+            oilSensor.Injector = DeterministicInjector(commonArgs{:}, specificArgs{:});
+
+        case "MCV-3"
+            altSensor.Injector = DeterministicInjector(...
+                "NoiseSigma", 0.3, ...
+                "FaultRatePerSecond", 0.1, ...
+                "W_Stuck", 1, ...
+                "W_Bias", 1, ...
+                "W_Drift", 1, ...
+                "W_Dropout", 1, ...
+                "W_Spike", 1 ...
+                ); 
+
+            vsSensor.Injector = DeterministicInjector(...
+                "NoiseSigma", 0.3, ...
+                "FaultRatePerSecond", 0.1, ...
+                "W_Stuck", 1, ...
+                "W_Bias", 1, ...
+                "W_Drift", 1, ...
+                "W_Dropout", 1, ...
+                "W_Spike", 1 ...
+                );
+
+            asSensor.Injector = DeterministicInjector(...
+                "NoiseSigma", 0.3, ...
+                "FaultRatePerSecond", 0.1, ...
+                "W_Stuck", 1, ...
+                "W_Bias", 1, ...
+                "W_Drift", 1, ...
+                "W_Dropout", 1, ...
+                "W_Spike", 1 ...
+                );
+
+            pitchSensor.Injector = DeterministicInjector(...
+                "NoiseSigma", 0.3, ...
+                "FaultRatePerSecond", 0.1, ...
+                "W_Stuck", 1, ...
+                "W_Bias", 1, ...
+                "W_Drift", 1, ...
+                "W_Dropout", 1, ...
+                "W_Spike", 1 ...
+                );
+
+            rollSensor.Injector = DeterministicInjector(...
+                "NoiseSigma", 0.3, ...
+                "FaultRatePerSecond", 0.1, ...
+                "W_Stuck", 1, ...
+                "W_Bias", 1, ...
+                "W_Drift", 1, ...
+                "W_Dropout", 1, ...
+                "W_Spike", 1 ...
+                );
+
+            tempSensor.Injector = DeterministicInjector(...
+                "NoiseSigma", 0.3, ...
+                "FaultRatePerSecond", 0.1, ...
+                "W_Stuck", 1, ...
+                "W_Bias", 1, ...
+                "W_Drift", 1, ...
+                "W_Dropout", 1, ...
+                "W_Spike", 1 ...
+                );
+
+            oilSensor.Injector = DeterminsticInjector(...
+                "NoiseSigma", 0.3, ...
+                "FaultRatePerSecond", 0.1, ...
+                "W_Stuck", 1, ...
+                "W_Bias", 1, ...
+                "W_Drift", 1, ...
+                "W_Dropout", 1, ...
+                "W_Spike", 1 ...
+                );
     
         otherwise
             error("Unknown scenarioId: %s", scenarioId);
@@ -135,21 +311,40 @@ for run = 1:N
         sensorStates(k,:) = [altS, asS, vsS, pitS, rolS, tmpS, oilS];
         systemStates(k) = state;
     
-        % Store ground-truth injected fault class for BV-4 scenarios
-        if scenarioId == "BV-4a"
-            classifiedStates(k) = FaultClassifier(asMeta);
-        elseif scenarioId == "BV-4b"
-            classifiedStates(k) = FaultClassifier(pitMeta);
-        elseif scenarioId == "BV-4c"
-            classifiedStates(k) = FaultClassifier(rolMeta);
-        elseif scenarioId == "BV-4d"
-            classifiedStates(k) = FaultClassifier(oilMeta);
-        end
-    
         fprintf("t=%.2f  SYS=%s  ALT=%s  VS=%s  AS=%s  PIT=%s  ROL=%s  TMP=%s  OIL=%s\n", ...
             tt, state, altS, vsS, asS, pitS, rolS, tmpS, oilS);
+
+        if state == "WARN"
+            warnPresent(k) = 1; 
+        end
     end
+
+    for i = 2:length(sysStateNum)
+
+        % Detect entry into WARN
+        if sysStateNum(i) == 1 && sysStateNum(i-1) ~= 1
+            warnEvents = warnEvents + 1;
+            detectionTime = k; 
+        end
     
+        % Detect entry into FAULT
+        if sysStateNum(i) == 2 && sysStateNum(i-1) ~= 2
+            faultEvents = faultEvents + 1;
+            detectionTime = k; 
+        end
+    end
+
+    %% Store overall state for plotting after simulation 
+    if any(sysStateNum == 1)
+        warnOccured(run) = 1; 
+    end
+
+    if any(sysStateNum == 2)
+        faultOccured(run) = 1;  
+    end 
+
+    warnTimesteps = [warnTimesteps; warnPresent]; 
+
     %% Calculate the rate of change of the altitude
     altSlope_hist = nan(length(t),1);
     
@@ -160,94 +355,50 @@ for run = 1:N
     end
 end
 
-if scenarioId == "BV-1" || scenarioId == "BV-2" || scenarioId =="BV-3"
-    %% Convert sensor states to numeric values for plotting
-    stateMap = containers.Map(["OK","SUSPECT","FAILED"], [0 1 2]);
-    numStates = zeros(length(t), 7);
+totalTimesteps = N * length(t);
+
+warnEventRate = warnEvents / totalTimesteps;
+faultEventRate = faultEvents / totalTimesteps;
+
+warnRateRuns = (sum(warnOccured == 1))/N;
+warnRateTimestepsRows = sum(warnTimesteps == 1); 
+warnRateTimesteps = (sum(warnRateTimestepsRows))/(numel(warnTimesteps)); 
+faultRate = (sum(faultOccured == 1))/N; 
+
+fprintf("Monte Carlo Results for %d runs in scenario %s:\n", N, scenarioId)
+
+fprintf("False WARN events: %d\n", warnEvents)
+fprintf("WARN event rate: %.5f\n", warnEventRate)
+
+fprintf("FAULT events: %d\n", faultEvents)
+fprintf("FAULT event rate (runs): %.3f\n", faultEventRate)
+
+if scenarioId == "MCV-1"
+    % Make Graph(s) showing noise distribution and need to show % which caused warnings/fails etc.  
     
-    for i = 1:length(t)
-        for j = 1:7
-            numStates(i,j) = stateMap(sensorStates(i,j));
-        end
-    end
-    
-    %% Plot sensor health states over time
-    sensorNames = ["Altitude","Airspeed","VertSpeed","Pitch","Roll","Temp","Oil"];
-    
+    % Hiustogram of warning outcomes
+
+    % Bar chart of warning/fault rates
     figure
-    
-    subplot(2,1,1)
-    hold on
-    for i = 1:7
-        offset = (i-1)*3;
-        stairs(t, numStates(:,i) + offset, 'LineWidth', 2)
-    end
-    
-    % Create tick labels
-    stateLabels = ["OK","SUSPECT","FAILED"];
-    yticks(0:1:(7*3-1))
-    
-    labels = strings(21,1);
-    idx = 1;
-    
-    for s = 1:7
-        for st = 1:3
-            labels(idx) = sensorNames(s) + " - " + stateLabels(st);
-            idx = idx + 1;
-        end
-    end
-    
-    yticklabels(labels)
-    
-    if scenarioId == "BV-2" || scenarioId == "BV-3" || scenarioId == "BV-4a" || ...
-       scenarioId == "BV-4b" || scenarioId == "BV-4c" || scenarioId == "BV-4d" || ...
-       scenarioId == "BV-5"
-        xline(1.0, '--r', 'Fault injected')
-    end
-    
-    xlabel("Time (s)")
-    ylabel("Sensor State")
-    title(sprintf("Sensor Health States During Scenario %s", scenarioId))
+    bar([warnEventRate*100 faultEventRate*100])
+    ylabel("Percentage (%)")
+    xticklabels(["False WARN","False FAULT"])
+    title("Monte Carlo Robustness Results under 1000 runs, treating WARN and FAULT transitions as event-driven")
+    annotation("textbox", ...
+    [0.65 0.6 0.25 0.25], ...
+    "String", { ...
+    "Noise model:", ...
+    "Altitude σ = 0.3 m", ...
+    "Vertical Speed σ = 0.1 m/s", ...
+    "Airspeed σ = 0.1 m/s", ...
+    "Pitch σ = 0.3°", ...
+    "Roll σ = 0.3°", ...
+    "Temp σ = 0.5 °C", ...
+    "Oil σ = 0.2 bar"}, ...
+    "FitBoxToText","on");
     grid on
-    %% Overall System State Graph
 
-    subplot(2,1,2)
-    % Plot OVERALL State
-    stairs(t, sysStateNum, 'LineWidth', 2)
-
-    % add fault injected marker
-    xline(1.0, '--r', 'Fault injected')
-
-    % Get states
-    yticks([0 1 2])
-    yticklabels(["NORMAL", "WARN", "FAULT"])
-
-    % Add labels to diagram
-    xlabel("Time (s)")
-    ylabel("System State")
-    title("Overall System State During BV-3")
-
-    grid on
-end
-
-%% Plot fault classification for BV-4 scenarios
-if scenarioId == "BV-4a" || scenarioId == "BV-4b" || scenarioId == "BV-4c" || scenarioId == "BV-4d"
-
-    failureClassificationNames = ["Normal", "Stuck-at", "Bias", "Drift", "Spike", "Dropout"];
-
-    figure
-    stairs(t, classifiedStates, 'LineWidth', 2, 'Marker', 'o')
-
-    xline(1.0, '--r', 'Fault Injected')
-
-    yticks(0:5)
-    yticklabels(failureClassificationNames)
-
-    ylabel("Injected Fault Classification")
-    xlabel("Time (s)")
-    title(sprintf("Injected Fault Classification During Scenario %s", scenarioId))
-    grid on
-end
+end 
 
 disp("Scenario complete.");
 
@@ -292,31 +443,4 @@ function classId = FaultClassifier(meta)
         otherwise
             classId = 0;
     end
-end
-
-if scenarioId == "BV-5"
-
-    figure
-
-    subplot(2,1,1)
-    plot(t, vsC_hist, 'LineWidth', 2)
-    hold on
-    plot(t, altSlope_hist, 'LineWidth', 2)
-    xline(5.0, '--r', 'Fault injected')
-    xlabel("Time (s)")
-    ylabel("Rate")
-    title("Redundancy Mismatch Evidence")
-    legend("Vertical Speed", "d(Altitude)/dt", 'Location', 'best')
-    grid on
-
-    subplot(2,1,2)
-    stairs(t, sysStateNum, 'LineWidth', 2)
-    xline(5.0, '--r', 'Fault injected')
-    yticks([0 1 2])
-    yticklabels(["NORMAL","WARN","FAULT"])
-    xlabel("Time (s)")
-    ylabel("System State")
-    title("Orchestrator State")
-    grid on
-
 end
